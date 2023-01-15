947a0aa304e5bf67ba701e3e6754350fe1dc55e3
Marco Ghilardi <marco.ghilardi@ralphlauren.com>
2023-01-15 13:12:02 +0000

    Use of Navigation container for more than one page

--- App.js
+++ App.js
@@ -1,13 +1,32 @@
+// Common Components
+import React, { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+// Custom Components
+import OpeningScreen from './src/screens/OpeningScreen';
 import HomeScreen from './src/screens/HomeScreen';
 
 export default function App() {
+  const theme = {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+      border: "transparent",
+    }
+  }
+
+  // useEffect(() => {
+  //   SplashScreen.hide(); //hides the splash screen on app load.
+  // }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer theme={theme}>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
 
