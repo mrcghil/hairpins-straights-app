5ccdb92f6cc3c9d0006188bd5c6cf973670ad438
Marco Ghilardi <marco.ghilardi@ralphlauren.com>
2023-01-15 12:29:35 +0000

    renamed

---
+++ src/components/EpisodeItem.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  View,
+  FlatList,
+  StyleSheet,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const EpisodeItem = () => {
+
+};
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      marginTop: StatusBar.currentHeight || 0,
+    },
+    item: {
+      backgroundColor: '#f9c2ff',
+      padding: 20,
+      marginVertical: 8,
+      marginHorizontal: 16,
+    },
+    title: {
+      fontSize: 32,
+    },
+  });
+
+export default EpisodeItem;
+
