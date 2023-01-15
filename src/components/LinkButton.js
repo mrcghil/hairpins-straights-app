159642b96278199e707d11f46192c01256fcd727
Marco Ghilardi <marco.ghilardi@ralphlauren.com>
2023-01-15 12:54:38 +0000

    renamed for external links

---
+++ src/components/LinkButton.js
@@ -0,0 +1,21 @@
+import { StyleSheet, Text } from 'react-native';
+
+const LinkButton = () => {
+    return(Null)
+};
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      marginTop: StatusBar.currentHeight || 0,
+    },
+    text: {
+      color: "#FFFFFF",
+      backgroundColor: "#FF0000",
+      padding: 20,
+      marginVertical: 8,
+      fontSize: 32,
+    }
+});
+
+export default LinkButton;
